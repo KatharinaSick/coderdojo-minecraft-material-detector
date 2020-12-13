a264b256e8bcebcd115b81d8918f4ffddb90cbdb
BuildTools <unconfigured@null.spigotmc.org>
2020-12-13 08:18:28 +0100

    code from 2nd workshop

--- src/main/java/MaterialDetector.java
+++ src/main/java/MaterialDetector.java
@@ -1,6 +1,13 @@
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MaterialDetector extends JavaPlugin {
@@ -8,6 +15,34 @@ public class MaterialDetector extends JavaPlugin {
     @Override
     public void onEnable() {
         super.onEnable();
+
+        getServer().getPluginManager().registerEvents(new PlayerEventListener(), this);
+
+        ItemStack itemStack = new ItemStack(Material.DIAMOND_PICKAXE);
+
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setDisplayName("Kohle Detektor");
+        itemMeta.setCustomModelData(258);
+        itemStack.setItemMeta(itemMeta);
+
+        itemStack.addEnchantment(Enchantment.DURABILITY, 3);
+        itemStack.addEnchantment(Enchantment.VANISHING_CURSE, 1);
+
+        NamespacedKey key = new NamespacedKey(this, "coal-detector");
+        ShapedRecipe recipe = new ShapedRecipe(key, itemStack);
+
+        recipe.shape(
+                "C  ",
+                " P ",
+                "KKK"
+        );
+
+        recipe.setIngredient('C', Material.COMPASS);
+        recipe.setIngredient('P', Material.DIAMOND_PICKAXE);
+        recipe.setIngredient('K', Material.COAL);
+
+        Bukkit.addRecipe(recipe);
+
     }
 
     @Override
---
+++ src/main/java/PlayerEventListener.java
@@ -0,0 +1,13 @@
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerEventListener implements Listener {
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        player.sendMessage("Hallo " + player.getName());
+    }
+}
